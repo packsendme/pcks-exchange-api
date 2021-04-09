95a2d6c276f116c867f243c5b428f55fc6d65a59
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2021-04-09 12:54:25 +0200

    ok

--- src/main/java/com/packsendme/api/exchange/controller/Exchange_Controller.java
+++ src/main/java/com/packsendme/api/exchange/controller/Exchange_Controller.java
@@ -1,11 +1,16 @@
 package com.packsendme.api.exchange.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +24,26 @@ public class Exchange_Controller {
 	@Autowired
 	private Exchange_Services exchange_Services; 
 	
+	private Map<String,Object> header = new HashMap<String,Object>();
 	
 	//========================================================================================
 	// METHOD POST|GET ::EXCHANGE-BRE -> CURRENT
 	//========================================================================================//
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/rate/{current}")
-	public ResponseEntity<?> getExchange(@Validated  @PathVariable ("current") String current) {		
+	public ResponseEntity<?> getExchange(
+			@RequestHeader("isoLanguageCode") String isoLanguageCode, 
+			@RequestHeader("isoCountryCode") String isoCountryCode,
+			@RequestHeader("isoCurrencyCode") String isoCurrencyCode,
+			@RequestHeader("originApp") String originApp,
+			@Validated  @PathVariable ("current") String current) {		
 		try {
+			header.put("isoLanguageCode", isoLanguageCode);
+			header.put("isoCountryCode", isoCountryCode);
+			header.put("isoCurrencyCode", isoCurrencyCode);
+			header.put("originApp", originApp);
+			
 			return exchange_Services.getExchangeRate(current);
 		} catch (Exception e) {
 			e.printStackTrace();
